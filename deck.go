1b046435ce7f2482e02139f5d6ad07a04dc8b647
Lilly Daniell <lilly.daniell@engineerbetter.com>
2022-05-24 14:20:30 +0100

    Remove comments

--- deck.go
+++ deck.go
@@ -2,10 +2,7 @@ package main
 
 import "fmt"
 
-// create a new type of 'deck'
-// which is a slice of strings
 type deck []string
-
 
 func newDeck() deck {
 	cards := deck{}
@@ -21,14 +18,8 @@ func newDeck() deck {
 	return cards
 }
 
-
-
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i + 1, card)
 	}
 }
-// d is the receiver, deck is the type, defined in deck.go
-// by convention, refer to the receiver with a one-two
-// letter abbreviation that matches the TYPE of the recevier
-// 'd' is a value of type 'deck'
