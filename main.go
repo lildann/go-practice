1815ea54df785c6b03655388fdfcc669a6f7501d
Lilly Daniell <lilly.daniell@engineerbetter.com>
2022-05-25 18:25:01 +0100

    Type conversion and strings.Join() function

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
 
-
 	hand, remainingCards := deal(cards, 5) // multiple return values from one function
-	hand.print()
+	fmt.Println(hand.toString())
 	remainingCards.print()
+
 }
 
+	// Type conversion to byte: fmt.Println([]byte("Hi There!")) -> [72 105 32 84 104 101 114 101 33]
